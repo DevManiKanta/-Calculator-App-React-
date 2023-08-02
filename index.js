66c4043656d3faa5c06a72739154d6e2f5c7e4fe
DevManiKanta <134351669+DevManiKanta@users.noreply.github.com>
2023-08-02 12:37:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import  ReactDOM from 'react-dom/client';
+import './index.css';
+import'bootstrap/dist/css/bootstrap.min.css';
+import App from './Components/FuncComponents/App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <div>
+    <App/>
+  
+  </div>
+);
+
